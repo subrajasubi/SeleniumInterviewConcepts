8e4e8371351c918647612f955bb6cff2ff85085c
subrajasubi <95363799+subrajasubi@users.noreply.github.com>
2022-03-01 20:58:18 +0530

    Add files via upload

---
+++ Frames.java
@@ -0,0 +1,84 @@
+package week1.day2.test;
+
+import static org.testng.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Frames {
+	
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver =  new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("http://www.leafground.com/pages/frame.html");
+		driver.switchTo().frame(0);
+		driver.findElement(By.id("Click")).click();
+		driver.switchTo().defaultContent();
+		driver.switchTo().frame(1);
+		driver.switchTo().frame("frame2");
+		driver.findElement(By.id("Click1")).click();
+		driver.switchTo().defaultContent();
+		List<WebElement> frames = driver.findElements(By.tagName("iframe"));
+		System.out.println("No : of Frames : " +frames.size());
+		
+		
+		
+		
+		
+	
+		}
+		
+	
+		}
+		
+
+	 
+
+
+
+
+			
+		
+        
+	    
+	
+
+	
+   
+    
+
+	
+	
+	
+
+
